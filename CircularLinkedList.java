b75854ee9a112b59f1d698b3f02b970f27d1bd5b
Anthony Do <47260573+anthonydo1@users.noreply.github.com>
2019-09-08 22:09:07 -0700

    Add files via upload

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -5,8 +5,17 @@ public class CircularLinkedList {
 	
 	private Node head;
 	private Node tail;
-	private int count;
+	private int size;
 	
+	
+	/**
+	 * Constructs an empty LinkedList.
+	 */
+	public CircularLinkedList() {
+		head = null;
+		tail = null;
+		size = 0;
+	}
 	
 	/**
 	 * Constructs a CircularLinkedList with n number of "prisoners" (nodes).
@@ -30,11 +39,11 @@ public class CircularLinkedList {
 	 * @return Returns the free prisoner.
 	 */
 	public Object getFreePrisoner(int num) {
-		System.out.println("Freeing prisoner...");
+		System.out.println("\nFreeing prisoner...");
 		this.printCircularLinkedList();
 		Node current = head;
 
-		while (count != 1) {
+		while (size != 1) {
 			for (int i = 0; i < num; i++) {
 				current = current.getNext();
 			}
@@ -45,7 +54,24 @@ public class CircularLinkedList {
 			this.printCircularLinkedList();
 		}
 		
+		System.out.println("Prisoner " + head.getData() + " was granted freedom.");
 		return head.getData();
+	}
+	
+	
+	/**
+	 * Inserts n number of nodes into the CircularLinkedList.
+	 * @param num The number of nodes to be inserted.
+	 */
+	public void insert(int num) {
+		if (num <= 0) num = 1;
+		
+		for (int i = 1; i <= num; i++) {
+			this.add(i);
+		}
+		
+		tail.setNext(head);
+		head.setPrev(tail);
 	}
 	
 	
@@ -68,7 +94,7 @@ public class CircularLinkedList {
 			tail = node;
 		}
 		
-		count++;
+		size++;
 	}
 	
 	
@@ -93,9 +119,29 @@ public class CircularLinkedList {
 			node.getNext().setPrev(node.getPrev());
 		}
 		
-		count--;
+		size--;
 	}
 	
+	
+	/**
+	 * Checks if the current CircularLinkedList is empty and return true/false.
+	 * @return True or False depending on if list is empty.
+	 */
+	public boolean isEmpty() {
+		if (head != null && size > 0) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * Returns the size of the CircularLinkedList.
+	 * @return size The size of the CircularLinkedList.
+	 */
+	public int getSize() {
+		return size;
+	}
 	
 	
 	/**
@@ -103,7 +149,7 @@ public class CircularLinkedList {
 	 */
 	public void printCircularLinkedList() {
 		Node current = head;
-		for (int i = 0; i < count; i++) {
+		for (int i = 0; i < size; i++) {
 			System.out.print(" " + current.getData() + " ->");
 			current = current.getNext();
 		}
@@ -112,24 +158,34 @@ public class CircularLinkedList {
 	
 	
 	public static void main(String[] args) {
+		double start = System.currentTimeMillis();
 		CircularLinkedList linkedlist = new CircularLinkedList(6);
-		System.out.println("\nPrisoner " + linkedlist.getFreePrisoner(2) + " is granted freedom. Expected: 1 \n");
+		System.out.println("\nPrisoner " + linkedlist.getFreePrisoner(2) + " was granted freedom in " 
+				+ (System.currentTimeMillis() - start) + " milliseconds. \nExpected: 1 ");
 		System.out.println("-------------------------------------------------");
 		
+		double start2 = System.currentTimeMillis();
 		CircularLinkedList linkedlist2 = new CircularLinkedList(1);
-		System.out.println("\nPrisoner " + linkedlist2.getFreePrisoner(9) + " is granted freedom. Expected: 1 \n");
+		System.out.println("\nPrisoner " + linkedlist2.getFreePrisoner(9) + " was granted freedom in " 
+				+ (System.currentTimeMillis() - start2) + " milliseconds. \nExpected: 1 ");
 		System.out.println("-------------------------------------------------");
 		
+		double start3 = System.currentTimeMillis();
 		CircularLinkedList linkedlist3 = new CircularLinkedList(7);
-		System.out.println("\nPrisoner " + linkedlist3.getFreePrisoner(7) + " is granted freedom. Expected: 4 \n");
+		System.out.println("\nPrisoner " + linkedlist3.getFreePrisoner(7) + " was granted freedom in " 
+				+ (System.currentTimeMillis() - start3) + " milliseconds. \nExpected: 4 ");
 		System.out.println("-------------------------------------------------");
 		
+		double start4 = System.currentTimeMillis();
 		CircularLinkedList linkedlist4 = new CircularLinkedList(12);
-		System.out.println("\nPrisoner " + linkedlist4.getFreePrisoner(8) + " is granted freedom. Expected: 2 \n");
+		System.out.println("\nPrisoner " + linkedlist4.getFreePrisoner(8) + " was granted freedom in " 
+				+ (System.currentTimeMillis() - start4) + " milliseconds. \nExpected: 2 ");
 		System.out.println("-------------------------------------------------");
 		
+		double start5 = System.currentTimeMillis();
 		CircularLinkedList linkedlist5 = new CircularLinkedList(5);
-		System.out.println("\nPrisoner " + linkedlist5.getFreePrisoner(1) + " is granted freedom. Expected: 3 \n");
+		System.out.println("\nPrisoner " + linkedlist5.getFreePrisoner(1) + " was granted freedom in " 
+				+ (System.currentTimeMillis() - start5) + " milliseconds. \nExpected: 3 ");
 		System.out.println("-------------------------------------------------");
 	}
 	
